ae983e0fb7628542bd426f97eb0a1248ce0fda91
antataiv <antata_iv@abv.bg>
2015-11-02 16:57:43 +0200

    contest details enhancement and voting for images

--- PhotoContest/PhotoContest.Web/Controllers/ImageController.cs
+++ PhotoContest/PhotoContest.Web/Controllers/ImageController.cs
@@ -12,6 +12,7 @@ using System.Linq;
 using PagedList;
 using PagedList.Mvc;
 using PhotoContest.Web.ViewModels;
+using PhotoContest.Models;
 
 namespace PhotoContest.Web.Controllers
 {
@@ -39,6 +40,41 @@ namespace PhotoContest.Web.Controllers
 
 
             return null;
+        }
+
+        public ActionResult Vote(int imageId)
+        {
+            var image = this.Data.Images
+                            .All()
+                            .FirstOrDefault(i => i.Id == imageId);
+
+            var currUserId = this.User.Identity.GetUserId();
+
+            var currUser = this.Data.Users
+                                .All()
+                                .FirstOrDefault(u => u.Id == currUserId);
+
+            if (image != null)
+            {
+                var userHasVoted = image.Ratings.Any(r => r.Author.Id == currUser.Id);
+                if (!userHasVoted)
+                {
+                    this.Data.Ratings.Add(new Rating
+                    {
+                        ImageId = image.Id,
+                        Author = currUser,
+                        Value = 1
+                    });
+
+                    this.Data.SaveChanges();
+                }
+
+                var votesCount = image.Ratings.Sum(v => v.Value);
+
+                return this.Content(votesCount.ToString());
+            }
+
+            return new EmptyResult();
         }
     }
 }
